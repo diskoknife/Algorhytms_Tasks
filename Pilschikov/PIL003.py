848dc091994bc9093b1fed09deda94c3776287f7
diskoknife <diskoknife@gmail.com>
2021-01-17 19:20:09 +0300

    maek TODO notice

--- Pilschikov/PIL003.py
+++ Pilschikov/PIL003.py
@@ -1,1 +1,18 @@
 # table of TRUTH, page 26, task 5.30
+a = bool(0)
+b = bool(0)
+c = bool(0)
+
+def f_x(a, b, c):
+    f = bool((a and b) or not (b or c))
+    return f
+
+
+if not a:
+    if not b:
+        if not c:
+            table_truth = [a, b, c, f_x(a, b, c)]
+            c = not c
+
+
+print(table_truth)
