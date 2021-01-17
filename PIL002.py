e0977d0b7845afd96f28fa5bb73bfb9301a2ebe6
diskoknife <diskoknife@gmail.com>
2021-01-17 18:02:52 +0300

    add sum of fibonaccis nums

--- PIL002.py
+++ PIL002.py
@@ -9,6 +9,7 @@ print("difference between min and max is", ans)
 print("enter the ordinal of needed Fibonacci's number")
 n = int(input())
 fib_seq = []
+fib_sum = int()
 x = 0
 y = 1
 z = 0
@@ -24,10 +25,15 @@ for i in range(n):
     y = fib_seq[i]
     if y > m and z == 0:
         z = fib_seq[i]
+    print("|",fib_seq[i], "|")
+    if fib_seq[i] < 1000:
+        fib_sum = fib_sum + fib_seq[i]
 
 
-print(fib_seq)
+
 if z != 0:
     print("first num that bigger than yours:", z)
 else:
     print("There is no bigger nums than yours")
+
+print("Sum of all Fibonacci's sequence above 1000 nums in your sequence:", fib_sum)
