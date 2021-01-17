372f06ffd173483df80d8727ffb7f30f95c08ad9
diskoknife <diskoknife@gmail.com>
2021-01-17 17:40:19 +0300

    add few tasks from 5 paragraph

--- PIL002.py
+++ PIL002.py
@@ -4,3 +4,21 @@ import random
 x_list = [random.randrange(1, 100000) for i in range(1, 100)]
 ans = max(x_list) - min(x_list)
 print("difference between min and max is", ans)
+
+# Task 5.16 | Fibonacci's numbers
+print("enter the ordinal of needed Fibonacci's number")
+n = int(input())
+fib_seq = []
+x = 0
+y = 1
+
+for i in range(n):
+    fib_seq.append(x+y)
+    try:
+        x = fib_seq[i-1]
+    except:
+        pass
+    y = fib_seq[i]
+
+
+print(fib_seq)
