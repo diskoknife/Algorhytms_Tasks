5932b5a41a56e5f022d1ca2fe1e5c1dc11b9afc5
diskoknife <diskoknife@gmail.com>
2021-01-25 15:25:03 +0300

    task with date

--- Pilschikov/PIL006.py
+++ Pilschikov/PIL006.py
@@ -1,4 +1,14 @@
 # task 8.1 page 40:
 # fill some arrays and show num of elements
 # show type of values
-# show the link to first and last element""
+# show the link to first and last element"
+from datetime import date
+
+day = ['Yesterday', 'Today', 'Tomorrow']
+vec = [x for x in range(31)]
+print(vec)
+print(day[1], date.today())
+
+print(type(day))
+print(type(vec))
+print(date)
