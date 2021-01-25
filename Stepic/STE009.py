4fc77b85cda2866ed09293b51c554f52aee39c93
diskoknife <diskoknife@gmail.com>
2021-01-25 14:52:28 +0300

    Added recursive task

---
+++ Stepic/STE009.py
@@ -0,0 +1,17 @@
+# recursive task
+# calculate the number of combinations from n to k
+# formula: C(n, k) = C(n-1, k) + C(n-1, k-1)
+
+def combnk(n, k):
+    if k == 0 or k > n:
+        return 0
+    elif k is n:
+        return 1
+    else:
+        cnk = combnk(n-1, k) + combnk(n - 1, k - 1)
+        return cnk
+
+
+n, k = map(int, input().split())  # read two numbers splitted by space
+
+print(combnk(n+1, k+1))
