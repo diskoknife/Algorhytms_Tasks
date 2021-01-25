3f8af83a085b2572d0694b0159ac22c7602bee24
diskoknife <diskoknife@gmail.com>
2021-01-25 14:10:07 +0300

    Fibonacci'' num

---
+++ Stepic/STE008.py
@@ -0,0 +1,11 @@
+# recursive function (Fibonacci's numbers)
+
+def fib(x):
+    if x == 0 or x == 1:
+        return 1
+    else:
+        return fib(x - 1) + fib(x - 2)
+
+
+x = int(input("Enter index number of needed Fibonacci's numeral:"))
+print(fib(x))
