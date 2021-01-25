1b47b5eed8900ce48609e80e6e0c0c40f6081528
diskoknife <diskoknife@gmail.com>
2021-01-25 11:13:31 +0300

    I dunno how to put answer in questions to code

--- Pilschikov/PIL004.py
+++ Pilschikov/PIL004.py
@@ -1,3 +1,17 @@
 # task 7.1 page 33
 
-season = ('winter', 'spring', 'summer', 'fall')
+season = ('winter', 'spring', 'summer', 'fall')  # make tuple cuz we have fixed num of seasons in the year
+temp = ('cold', 'warm')  # here we use tuple to interpretate pascal
+
+x = season[1]
+y = x
+t = temp[1]
+# next 2 questions is unexpected tokens... So
+try:
+    t = temp['hot']
+except:
+    print('únavaiable value')
+print(x, y, t)
+
+ex_7_1_b = [season[1] < season[2], season[0] <= season[2]]
+print(ex_7_1_b)
