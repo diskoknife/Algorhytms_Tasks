9bc6e5730d5c4e6c9d079fa0e43dabea60f80a4d
Vyacheslav Shchurov <vschyurov@psystems.biz>
2021-02-05 21:12:47 -0500

    Added inheritance exercise

---
+++ Stepic/STE012.py
@@ -0,0 +1,18 @@
+"""
+
+Extend list class as stack. Add to it possibility to sum/sub/multiply/divide 2 top elems
+
+"""
+
+class ExtendedStack(list):
+    def sum(self):
+        self.append(self.pop() + self.pop())
+
+    def sub(self):
+        self.append(self.pop() - self.pop())
+
+    def mul(self):
+        self.append(self.pop() * self.pop())
+
+    def div(self):
+        self.append(self.pop() // self.pop())
