fef8845458e0e3cb4ca1d86fc2dde652745460a9
Vyacheslav Shchurov <vschyurov@psystems.biz>
2021-02-08 00:45:38 -0500

    Added class and throw exception exercises

---
+++ Stepic/STE014.py
@@ -0,0 +1,16 @@
+"""
+
+Write a PositiveList class which inherit from list class and can append only positive nums
+Also write a NonPositiveException which throwing when you try  to add non positive num to list
+
+"""
+
+class NonPositiveError(Exception):
+    pass
+
+class PositiveList(list):
+    def append(self, x):
+        if x > 0:
+            super(PositiveList, self).append(x)
+        else:
+            raise NonPositiveError
