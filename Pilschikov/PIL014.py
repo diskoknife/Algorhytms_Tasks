9eaaeff5b90f5cd4c058e8876651010ba701e745
Vyacheslav Shchurov <vschyurov@psystems.biz>
2021-02-20 08:55:55 -0500

    added task from 16 paragraph

---
+++ Pilschikov/PIL014.py
@@ -0,0 +1,18 @@
+"""
+
+Task 16.18 page 103
+Something about lists
+
+"""
+
+l = []
+e = "elem"
+l.insert(0, e)  #task A
+l.append(e)  #task B
+l.insert(1, e)  #task C
+e1 = "new elem"
+l = [e + e1 for e in l if e == 'elem']  #task D
+e2 = 'newfoundelem'
+l.extend(e1 + e2)  #task E
+l.sort()  #last task
+print(l)
