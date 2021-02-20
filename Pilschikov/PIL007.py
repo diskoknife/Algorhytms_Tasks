fac3298ad3ae17fcb120332de8398be718f314de
Vyacheslav Shchurov <vschyurov@psystems.biz>
2021-02-20 06:49:02 -0500

    added task C to same file (maybe you should turn it into func?)

--- Pilschikov/PIL007.py
+++ Pilschikov/PIL007.py
@@ -6,13 +6,18 @@ fill array with integers
 """
 n = 10
 matrix1 = [[0 for i in range(n)] for i in range(n)]
-for i in range(n):          #task A
+"""for i in range(n):          #task A
     matrix1[i][i] = i
-    print(matrix1[i])
+    print(matrix1[i])"""
 
-loc = 1         #taskB
+"""loc = 1         #taskB
 for i in range(n):
     for j in range(n):
         matrix1[i][j] = loc
         loc +=1
-    print(matrix1[i])
+    print(matrix1[i])"""
+
+for i in range(n):      #taskC
+    for j in range(n):
+        matrix1[i][9-j] = 10-i-j
+print(matrix1)
