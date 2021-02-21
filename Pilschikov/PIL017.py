c105a8d804b37b8efcf5c72efdebc2e3847f2a8f
Vyacheslav Shchurov <vschyurov@psystems.biz>
2021-02-21 01:55:41 -0500

    Fix mistake with num of days

--- Pilschikov/PIL017.py
+++ Pilschikov/PIL017.py
@@ -9,8 +9,16 @@ months = ('January', 'February', 'Marth', 'April', 'May', 'June',
           'July', 'August', 'September', 'October', 'November', 'December')
 days = (28, 30, 31)
 datedict = {months[0]:days[2],
-            months[1]:days[0]}
-for i in range(2,12):
-    datedict.update({months[i]: days[2 if i%2 == 0 else 1]})
+            months[1]:days[0],
+            months[2]:days[2],
+            months[3]:days[1],
+            months[4]:days[2],
+            months[5]:days[1],
+            months[6]:days[2],
+            months[7]:days[2],
+            months[8]:days[1],
+            months[9]:days[2],
+            months[10]:days[1],
+            months[11]:days[2]}
 
 print(datedict)
