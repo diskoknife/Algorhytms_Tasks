81c04d15b0f5a2e9861518f0c81fb98572439e55
Vyacheslav Shchurov <vschyurov@psystems.biz>
2021-02-21 06:27:17 -0500

    Started new exercise with openfile and stack tasks

---
+++ Pilschikov/PIL023.py
@@ -0,0 +1,18 @@
+"""
+
+Task 17.5
+Eead the text from file, read the circle braces and print their nums in text
+for every braces pair
+
+"""
+
+braces = []
+num_braces = []
+
+with open("PIL023.txt", "r") as f:
+    text = f.read()
+    for i in range(len(text)):
+        if text[i] in ['(', ')']:
+            braces.append(text[i])
+            num_braces.append(i)
+
