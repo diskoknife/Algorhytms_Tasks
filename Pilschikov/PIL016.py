5512041153d75d3b2fbe8bf22d57b6a7b9696cee
Vyacheslav Shchurov <vschyurov@psystems.biz>
2021-02-21 01:36:21 -0500

    Added task from 12 paragraph

---
+++ Pilschikov/PIL016.py
@@ -0,0 +1,19 @@
+"""
+
+Exercise 12.12, page 73
+Calculate num of numbers in the text in the input file.
+
+"""
+
+def calc(input_file):
+    f = open(input_file, 'r')
+    inp_str = f.read()
+    count = 0
+    num_tuple = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '0')
+    for i in range(len(inp_str)):
+        if inp_str[i] in num_tuple:
+            count += 1
+    f.close()
+    return count
+
+print(calc('sample.txt'))
