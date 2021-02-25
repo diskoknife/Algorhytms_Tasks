3e3d43f68b89dc26ac88bd644586d01f685ba118
Vyacheslav Shchurov <vschyurov@psystems.biz>
2021-02-24 23:07:13 -0500

    delete(trash): PURGE THE UNCLEAN FILES

--- Pilschikov/PIL024.py
+++
@@ -1,8 +0,0 @@
-#  Copyright (c) 2021.
-#  Vyacheslav Shchurov (diskoknife@gmail.com)
-"""
-
-Task 6.18, page 31
-
-
-"""
--- Pilschikov/PIL025.py
+++
@@ -1,2 +0,0 @@
-#  Copyright (c) 2021.
-#  Vyacheslav Shchurov (diskoknife@gmail.com)
