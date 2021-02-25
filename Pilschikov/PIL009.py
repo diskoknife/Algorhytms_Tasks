97db189fe3d962edaba8b810cc871222972b9c08
Vyacheslav Shchurov <vschyurov@psystems.biz>
2021-02-24 22:49:31 -0500

    feat(allfile): fixed UX

--- Pilschikov/PIL009.py
+++ Pilschikov/PIL009.py
@@ -7,11 +7,12 @@ Calculate sum of arrays based on descriptioñ
 
 """
 from random import randint
+
 list1 = [randint(-1000, 1000) for i in range(20)]
 list2 = [randint(-1000, 1000) for i in range(20)]
 res1 = 0
 for i in range(0, 15):
     res1 += list1[i] + list2[i]
 
-u = sum(list1)**2 if res1 > 0 else sum(list2, 10)**2
-print(list1, res1, u)
+u = sum(list1) ** 2 if res1 > 0 else sum(list2, 10) ** 2
+print(list1, '\n', list2, '\n', res1, '\n', u)
