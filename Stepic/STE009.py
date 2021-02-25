02a24cc67699813dad757e43700d59d5f51a48d1
Vyacheslav Shchurov <vschyurov@psystems.biz>
2021-02-24 23:14:47 -0500

    feat(input): refactored code && added some UX

--- Stepic/STE009.py
+++ Stepic/STE009.py
@@ -6,16 +6,18 @@ Formula: C(n, k) = C(n-1, k) + C(n-1, k-1)
 
 """
 
+
 def combnk(n, k):
     if k == 0 or k > n:
         return 0
     elif k is n:
         return 1
     else:
-        cnk = combnk(n-1, k) + combnk(n - 1, k - 1)
+        cnk = combnk(n - 1, k) + combnk(n - 1, k - 1)
         return cnk
 
 
-n, k = map(int, input().split())  # read two numbers splitted by space
-
-print(combnk(n+1, k+1))
+if __name__ == '__main__':
+    n, k = map(int, input("Enter the num of elems and k replacements")
+               .split())  # read two numbers splitted by space
+    print(combnk(n + 1, k + 1))
