09cbbb19ca77817df3bee5b594f2bca515b12aa1
Vyacheslav Shchurov <vschyurov@psystems.biz>
2021-02-24 23:04:31 -0500

    refactor(matrix): matr is now matrix! Wake up Neo!

--- Pilschikov/PIL018.py
+++ Pilschikov/PIL018.py
@@ -8,32 +8,34 @@ We have 7x4 matrix. Shuffle her elements to make bigger elem in left upper corne
 """
 import random
 
-def sort(matr):
-    for i in range(len(matr)):
-        horizontal_swap = True  #define bool to run while at least once
+
+def sort(matrix):
+    for i in range(len(matrix)):
+        horizontal_swap = True  # define bool to run while at least once
         while horizontal_swap:
-            horizontal_swap = False # if we don't swap anything - this is our last cycle
-            for j in range(len(matr[i])):
+            horizontal_swap = False  # if we don't swap anything - this is our last cycle
+            for j in range(len(matrix[i])):
                 try:
-                    if matr[i][j] < matr[i][j+1]:
+                    if matrix[i][j] < matrix[i][j + 1]:
                         horizontal_swap = True
-                        matr[i][j], matr[i][j+1] = matr[i][j+1], matr[i][j]
+                        matrix[i][j], matrix[i][j + 1] = matrix[i][j + 1], matrix[i][j]
                 except IndexError:
                     pass
     """Now do same swap for columns
     We already have max value in every row at [0] position"""
-    for i in range(len(matr)):
+    for i in range(len(matrix)):
         vertical_swap = True
         while vertical_swap:
             vertical_swap = False
             try:
-                if matr[i][0] < matr[i+1][0]:
+                if matrix[i][0] < matrix[i + 1][0]:
                     vertical_swap = True
-                    matr[i][0], matr[i+1][0] = matr[i+1][0], matr[i][0]
-            except:
+                    matrix[i][0], matrix[i + 1][0] = matrix[i + 1][0], matrix[i][0]
+            except IndexError:
                 pass
-    return matr
+    return matrix
 
-a = [[random.randrange(-100, 100)for i in range(7)] for i in range(4)]
+
+a = [[random.randrange(-100, 100) for i in range(7)] for i in range(4)]
 
 print(sort(a))
