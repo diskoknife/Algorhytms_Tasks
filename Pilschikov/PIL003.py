68ebef890cf48e012c338edff8661afc9939e84f
Vyacheslav Shchurov <vschyurov@psystems.biz>
2021-02-24 21:21:24 -0500

    refactor(main): All code turned into functions

--- Pilschikov/PIL003.py
+++ Pilschikov/PIL003.py
@@ -6,9 +6,14 @@ Table of TRUTH, page 26, task 5.30
 
 """
 
-a = bool(0)
-b = bool(0)
-c = bool(0)
+
+def main():
+    args = map(bool, input(print("Enter the three logical values of a, b, "
+                                 "c vars\n Supported only"
+                                 "'True' or 'False' or '1' or '0' definition")
+                           ).split())
+    print("Answer for (a&b)|!(b|c):\n")
+    print(f_x(*args))
 
 
 def f_x(a, b, c):  # here we define truth function by task
@@ -16,9 +21,5 @@ def f_x(a, b, c):  # here we define truth function by task
     return f
 
 
-table = ["{0:b}".format(i) for i in range(0, 8)] # here i want to fill truth table for 3 variables
-# TODO: find how to split integer to fill the table with booleans
-print(table)
-
-# TODO: make real table of truth please
-
+if __name__ == '__main__':
+    main()
