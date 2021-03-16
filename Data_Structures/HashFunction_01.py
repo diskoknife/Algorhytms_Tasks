60e0f3acf2a210bfd90805dc76f5dc54c7ef0bac
Vyacheslav Shchurov <vschyurov@psystems.biz>
2021-03-16 08:30:01 -0400

    description(HashFunction): fill the description to exercises

---
+++ Data_Structures/HashFunction_01.py
@@ -0,0 +1,24 @@
+#  Copyright (c) 2021.
+#  Vyacheslav Shchurov (diskoknife@gmail.com)
+
+"""
+
+Make a simple phone numbers book which can Add number. Delete number. Find
+number. Here's over the Stepik task. Additional from me:
+1) Book can be read from file where all of the phones can be written
+in next patterns:
+    a) +00000000000 client;
+    b) +0-000-000-000 client;
+    c) 123-45-67 client;
+    d) 1234567890 client.
+    count of cyphers = {5...12}
+    numbers must be in only  one style.
+    e.g.:
+    no hyphens, no pluses.
+2) Some control:
+    a) Exit from the program only if you print "exit" (file autosaves)
+    b) In start of program you have choice between open existing database or make new
+    c) If you print "number <client>" -> program shows number of client that you search
+    d) Onprint 'whois <number>' -> program shows name of phone number owner
+
+"""
