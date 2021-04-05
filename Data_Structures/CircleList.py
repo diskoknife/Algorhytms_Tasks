af29cb2c521b72e815ac01869d49536ae513cc3b
Vyacheslav Shchurov <vschyurov@psystems.biz>
2021-04-05 14:14:33 -0400

    Done CircularListClass

--- Data_Structures/CircleList.py
+++ Data_Structures/CircleList.py
@@ -19,3 +19,29 @@ class CreateList:
         self.head.next = self.tail
         self.tail.next = self.head
 
+    def add(self, data):
+        newNode = Node(data)
+        if self.head.data is None:
+            self.head = newNode
+            self.tail = newNode
+            newNode.next = self.head
+        else:
+            self.tail.next = newNode
+            self.tail = newNode
+            self.tail.next = self.head
+
+    def display(self):
+        current = self.head
+        if self.head is None:
+            print('List is empty')
+            return
+        else:
+            print("Nodes of the circular linked list: ")
+            print(current.data)
+            while(current.next != self.head):
+                current = current.next
+                print(current.data)
+
+
+
+class CircularLinkedList:
